4b214890f2c737635fc96e70ce620dfbb4821398
James Kennard <james.kennard@nationaltrust.org.uk>
2014-09-19 14:05:29 +0100

    Empty LoggerFactory

---
+++ src/main/java/uk/co/webamoeba/slf4j/junit/LoggerFactory.java
@@ -0,0 +1,8 @@
+package uk.co.webamoeba.slf4j.junit;
+
+/**
+ * @author James Kennard
+ */
+public class LoggerFactory {
+
+}
---
+++ src/test/java/uk/co/webamoeba/slf4j/junit/LoggerFactoryTest.java
@@ -0,0 +1,17 @@
+package uk.co.webamoeba.slf4j.junit;
+
+import org.junit.Test;
+
+/**
+ * @author James Kennard
+ */
+public class LoggerFactoryTest {
+
+	@Test
+	public void shouldInstantiate() {
+		new LoggerFactory();
+	}
+	
+	// TODO make LoggerFactory implement ILoggerFactory 
+	
+}
