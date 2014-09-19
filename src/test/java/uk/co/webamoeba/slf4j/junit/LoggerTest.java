7dcb2979f698edda6cec559dbb57eb6c394f486a
James Kennard <james.kennard@nationaltrust.org.uk>
2014-09-19 14:02:32 +0100

    Add Logger

---
+++ src/test/java/uk/co/webamoeba/slf4j/junit/LoggerTest.java
@@ -0,0 +1,17 @@
+package uk.co.webamoeba.slf4j.junit;
+
+import org.junit.Test;
+
+/**
+ * @author James Kennard
+ */
+public class LoggerTest {
+
+	@Test
+	public void shouldInstantiate() {
+		new Logger();
+	}
+	
+	// TODO make Logger implement org.slf4j.Logger
+	
+}
