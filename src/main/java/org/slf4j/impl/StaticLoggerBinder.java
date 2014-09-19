1b14b603b4953c63a84d8b49b7a37b8d794a9a43
James Kennard <james.kennard@nationaltrust.org.uk>
2014-09-19 13:57:17 +0100

    Empty StaticLoggerBinder

---
+++ src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -0,0 +1,8 @@
+package org.slf4j.impl;
+
+/**
+ * @author James Kennard
+ */
+public class StaticLoggerBinder {
+
+}
---
+++ src/test/java/org/slf4j/impl/StaticLoggerBinderTest.java
@@ -0,0 +1,15 @@
+package org.slf4j.impl;
+
+import org.junit.Test;
+
+/**
+ * @author James Kennard
+ */
+public class StaticLoggerBinderTest {
+
+	@Test
+	public void shouldInstantiate() {
+		new StaticLoggerBinder();
+	}
+	
+}
