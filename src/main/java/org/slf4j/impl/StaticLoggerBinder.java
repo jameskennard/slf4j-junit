5fa9c91e7d31177e8393f44b3ae994374c1eeaa9
James Kennard <james.kennard@nationaltrust.org.uk>
2014-09-19 16:36:31 +0100

    Implement StaticLoggerBinder

--- src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -1,8 +1,33 @@
 package org.slf4j.impl;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
+
+import uk.co.webamoeba.slf4j.junit.LoggerFactory;
+
 /**
  * @author James Kennard
  */
-public class StaticLoggerBinder {
+public class StaticLoggerBinder implements LoggerFactoryBinder {
+	
+	private static final StaticLoggerBinder STATIC_LOGGER_BINDER = new StaticLoggerBinder();
+
+	public static StaticLoggerBinder getSingleton() {
+		return STATIC_LOGGER_BINDER;
+	}
+	
+	/**
+	 * @see #getSingleton()
+	 */
+	private StaticLoggerBinder() {
+	}
+
+	public ILoggerFactory getLoggerFactory() {
+		return new LoggerFactory();
+	}
+
+	public String getLoggerFactoryClassStr() {
+		return LoggerFactory.class.getName();
+	}
 
 }
--- src/test/java/org/slf4j/impl/StaticLoggerBinderTest.java
+++ src/test/java/org/slf4j/impl/StaticLoggerBinderTest.java
@@ -1,6 +1,13 @@
 package org.slf4j.impl;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
 
 /**
  * @author James Kennard
@@ -8,8 +15,54 @@ import org.junit.Test;
 public class StaticLoggerBinderTest {
 
 	@Test
-	public void shouldInstantiate() {
-		new StaticLoggerBinder();
+	public void shouldGetSingleton() {
+		assertThat(StaticLoggerBinder.getSingleton(), is(notNullValue()));
 	}
 	
+	@Test
+	public void shouldGetSingletonGivenCalledMoreThanOnce() {
+		StaticLoggerBinder singletonInstance = StaticLoggerBinder.getSingleton();
+		StaticLoggerBinder anotherSingletonInstance = StaticLoggerBinder.getSingleton();
+		assertThat(singletonInstance, is(sameInstance(anotherSingletonInstance)));
+	}
+
+	@Test
+	public void shouldImplementLoggerFactoryBinder() {
+		// Given
+		StaticLoggerBinder binder = StaticLoggerBinder.getSingleton();
+
+		// When
+		boolean isLoggerFactoryBinder = LoggerFactoryBinder.class.isInstance(binder);
+
+		// Then
+		assertThat(isLoggerFactoryBinder, is(true));
+	}
+
+	@Test
+	public void shouldGetLoggerFactory() {
+		// Given
+		StaticLoggerBinder binder = StaticLoggerBinder.getSingleton();
+		String classString = binder.getLoggerFactoryClassStr();
+
+		// When
+		ILoggerFactory factory = binder.getLoggerFactory();
+
+		// Then
+		assertThat(factory, is(notNullValue()));
+		assertThat(factory.getClass().getName(), is(classString));
+	}
+
+	@Test
+	public void shouldGetLoggerFactoryClassStr() {
+		// Given
+		StaticLoggerBinder binder = StaticLoggerBinder.getSingleton();
+		String expectedClassString = "uk.co.webamoeba.slf4j.junit.LoggerFactory";
+
+		// When
+		String classString = binder.getLoggerFactoryClassStr();
+
+		// Then
+		assertThat(classString, is(expectedClassString));
+	}
+
 }
