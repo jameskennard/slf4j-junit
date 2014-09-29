cd03d955a23f63582a7e06621294e271e7f34c92
James Kennard <james.kennard@nationaltrust.org.uk>
2014-09-29 18:30:55 +0100

    Refactor test of matcher into common abstract test

--- src/test/java/uk/co/webamoeba/slf4j/junit/matcher/InfoLogEventMatcherTest.java
+++ src/test/java/uk/co/webamoeba/slf4j/junit/matcher/InfoLogEventMatcherTest.java
@@ -1,382 +1,82 @@
 package uk.co.webamoeba.slf4j.junit.matcher;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.StringDescription;
-import org.junit.Before;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.Marker;
-import org.slf4j.helpers.BasicMarkerFactory;
 
-import uk.co.webamoeba.slf4j.junit.event.LogEventRegistry;
-import uk.co.webamoeba.slf4j.junit.logger.RecordingLogger;
 
 /**
  * @author James Kennard
  */
-public class InfoLogEventMatcherTest {
+public class InfoLogEventMatcherTest extends LogEventMatcherTest {
 
-	@Before
-	public void clearAll() {
-		LogEventRegistry.getSingleton().clearAll();
+	@Override
+	protected InfoLogEventMatcher matcher() {
+		return new InfoLogEventMatcher("Message");
 	}
-
-	@Test
-	public void shouldImplementMatcher() {
-		assertThat(new InfoLogEventMatcher("Message"), is(instanceOf(Matcher.class)));
+	
+	@Override
+	protected InfoLogEventMatcher matcher(String message) {
+		return new InfoLogEventMatcher(message);
 	}
-
-	@Test
-	public void shouldNotMatchGivenUnexpectedTypeOfLogger() {
-		// Given
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher("Message");
-		Logger logger = mock(Logger.class);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertFalse(matches);
+	
+	@Override
+	protected LogEventMatcher matcher(String format, String argument) {
+		return new InfoLogEventMatcher(format, argument);
 	}
-
-	@Test
-	public void shouldDescribeToGivenUnexpectedTypeOfLogger() {
-		// Given
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher("Message");
-		Logger logger = mock(Logger.class);
-		Description description = new StringDescription();
-
-		// When
-		matcher.describeMismatch(logger, description);
-
-		// Then
-		assertThat(description.toString(), is("the item is not a test logger, it looks like you are not using the matcher correctly "));
+	
+	@Override
+	protected InfoLogEventMatcher matcher(Marker marker, String message) {
+		return new InfoLogEventMatcher(marker, message);
 	}
-
-	@Test
-	public void shouldMatchGivenLoggerHasMessage() {
-		// Given
-		String message = "Some Message";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message);
-		Logger logger = new RecordingLogger("A recording logger");
+	
+	@Override
+	protected InfoLogEventMatcher matcher(String message, Throwable throwable) {
+		return new InfoLogEventMatcher(message, throwable);
+	}
+	
+	@Override
+	protected InfoLogEventMatcher matcher(Marker marker, String message, Throwable throwable) {
+		return new InfoLogEventMatcher(marker, message, throwable);
+	}
+	
+	@Override
+	protected InfoLogEventMatcher matcher(Marker marker, String format, String argument) {
+		return new InfoLogEventMatcher(marker, format, argument);
+	}
+	
+	@Override
+	protected void log(String message, Logger logger) {
 		logger.info(message);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(true));
 	}
 	
-	@Test
-	public void shouldMatchGivenLoggerHasMarkerAndMessage() {
-		// Given
-		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
-		String message = "Some Message";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(marker, message);
-		Logger logger = new RecordingLogger("A recording logger");
+	@Override
+	protected void log(String format, String argument, Logger logger) {
+		logger.info(format, argument);
+	}
+	
+	@Override
+	protected InfoLogEventMatcher log(String format, String argument) {
+		return new InfoLogEventMatcher(format, argument);
+	}
+	
+	@Override
+	protected void log(Marker marker, String message, Logger logger) {
 		logger.info(marker, message);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(true));
 	}
 	
-	@Test
-	public void shouldMatchGivenLoggerHasMarkerAndFormattedMessage() {
-		// Given
-		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
-		String format = "Format {}";
-		String argument = "argument";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(marker, format, argument);
-		Logger logger = new RecordingLogger("A recording logger");
-		logger.info(marker, format, argument);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(true));
-	}
-	
-	@Test
-	public void shouldMatchGivenLoggerHasMessageAndThrowable() {
-		// Given
-		String message = "Some Message";
-		Throwable throwable = new Throwable();
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message, throwable);
-		Logger logger = new RecordingLogger("A recording logger");
+	@Override
+	protected void log(String message, Throwable throwable, Logger logger) {
 		logger.info(message, throwable);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(true));
 	}
 	
-	@Test
-	public void shouldMatchGivenLoggerHasMarkerAndMessageAndThrowable() {
-		// Given
-		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
-		String message = "Some Message";
-		Throwable throwable = new Throwable();
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(marker, message, throwable);
-		Logger logger = new RecordingLogger("A recording logger");
+	@Override
+	protected void log(Marker marker, String message, Throwable throwable, Logger logger) {
 		logger.info(marker, message, throwable);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(true));
 	}
 	
-	@Test
-	public void shouldNotMatchGivenLoggerHasMessageButNoMarker() {
-		// Given
-		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
-		String message = "Some Message";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(marker, message);
-		Logger logger = new RecordingLogger("A recording logger");
-		logger.info(message);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(false));
-	}
-	
-	@Test
-	public void shouldNotMatchGivenLoggerHasMarkerAndMessageButNoThrowable() {
-		// Given
-		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
-		String message = "Some Message";
-		Throwable throwable = new Throwable();
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(marker, message, throwable);
-		Logger logger = new RecordingLogger("A recording logger");
-		logger.info(marker, message);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(false));
-	}
-	
-	@Test
-	public void shouldNotMatchGivenLoggerHasMessageButNoThrowable() {
-		// Given
-		String message = "Some Message";
-		Throwable throwable = new Throwable();
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message, throwable);
-		Logger logger = new RecordingLogger("A recording logger");
-		logger.info(message);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(false));
-	}
-	
-	@Test
-	public void shouldNotMatchGivenLoggerHasMessageAndUnexpectedThrowable() {
-		// Given
-		String message = "Some Message";
-		Throwable throwable = new Throwable();
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message);
-		Logger logger = new RecordingLogger("A recording logger");
-		logger.info(message, throwable);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(false));
+	@Override
+	protected void log(Marker marker, String format, String argument, Logger logger) {
+		logger.info(marker, format, argument);
 	}
 
-	@Test
-	public void shouldMatchGivenLoggerHasStringMessageAndMatcherHasFormatedMessage() {
-		// Given
-		String message = "Some Message";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher("{}", message);
-		Logger logger = new RecordingLogger("A recording logger");
-		logger.info(message);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(true));
-	}
-
-	@Test
-	public void shouldMatchGivenLoggerHasFormattedMessageAndMatcherHasStringMessage() {
-		// Given
-		String message = "Some Message";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message);
-		Logger logger = new RecordingLogger("A recording logger");
-		logger.info("{}", message);
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(true));
-	}
-
-	@Test
-	public void shouldNotMatchGivenLoggerHasNoLogEvents() {
-		// Given
-		String message = "Some Message";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message);
-		Logger logger = new RecordingLogger("A recording logger");
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(false));
-	}
-
-	@Test
-	public void shouldNotMatchGivenLoggerHasLogEventWithDifferentMessage() {
-		// Given
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher("Some Message");
-		Logger logger = new RecordingLogger("A recording logger");
-		logger.info("Some Different Message");
-
-		// When
-		boolean matches = matcher.matches(logger);
-
-		// Then
-		assertThat(matches, is(false));
-	}
-
-	@Test
-	public void shouldDescribeMismatchGivenMessage() {
-		// Given
-		String message = "Some Message";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message);
-		String loggerName = "A recording logger";
-		Logger logger = new RecordingLogger(loggerName);
-		Description description = new StringDescription();
-
-		// When
-		matcher.describeMismatch(logger, description);
-
-		// Then
-		assertThat(description.toString(), is("info to \"" + loggerName + "\" with message \"" + message
-				+ "\" was not logged "));
-	}
-	
-	@Test
-	public void shouldDescribeMismatchGivenMessageAndThrowable() {
-		// Given
-		String message = "Some Message";
-		Throwable throwable = new Throwable();
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message, throwable);
-		String loggerName = "A recording logger";
-		Logger logger = new RecordingLogger(loggerName);
-		Description description = new StringDescription();
-
-		// When
-		matcher.describeMismatch(logger, description);
-
-		// Then
-		assertThat(description.toString(), is("info to \"" + loggerName + "\" with message \"" + message
-				+ "\" and throwable <" + throwable + "> was not logged "));
-	}
-	
-	@Test
-	public void shouldDescribeMismatchGivenSomethingElseWasLogged() {
-		// Given
-		String message = "Some Message";
-		String somethingElse = "Something Else";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message);
-		String loggerName = "A recording logger";
-		Logger logger = new RecordingLogger(loggerName);
-		logger.info(somethingElse);
-		Description description = new StringDescription();
-
-		// When
-		matcher.describeMismatch(logger, description);
-
-		// Then
-		assertThat(description.toString(), is("info to \"" + loggerName + "\" with message \"" + message
-				+ "\" was not logged; But these were logged info(\"" + somethingElse + "\") "));
-	}
-	
-	@Test
-	public void shouldDescribeMismatchGivenOtherThingsWasLogged() {
-		// Given
-		String message = "Some Message";
-		String anotherThing = "Another Thing";
-		String yetAnotherThing = "Yet Another Thing";
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message);
-		String loggerName = "A recording logger";
-		Logger logger = new RecordingLogger(loggerName);
-		logger.info(anotherThing);
-		logger.info(yetAnotherThing);
-		Description description = new StringDescription();
-
-		// When
-		matcher.describeMismatch(logger, description);
-
-		// Then
-		assertThat(description.toString(), is("info to \"" + loggerName + "\" with message \"" + message
-				+ "\" was not logged; But these were logged info(\"" + anotherThing + "\") info(\"" + yetAnotherThing + "\") "));
-	}
-
-	@Test
-	public void shouldDescribeToGivenStringMessage() {
-		// Given
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher("Some Message");
-		Description description = new StringDescription();
-
-		// When
-		matcher.describeTo(description);
-
-		// Then
-		assertThat(description.toString(), is("loggedInfo(\"Some Message\")"));
-	}
-	
-	@Test
-	public void shouldDescribeToGivenStringMessageAndThrowable() {
-		// Given
-		String message = "Some Message";
-		Throwable throwable = new Throwable();
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher(message, throwable);
-		Description description = new StringDescription();
-
-		// When
-		matcher.describeTo(description);
-
-		// Then
-		assertThat(description.toString(), is("loggedInfo(\"Some Message\", <" + throwable + ">)"));
-	}
-	
-	@Test
-	public void shouldDescribeToGivenFormattedMessage() {
-		// Given
-		InfoLogEventMatcher matcher = new InfoLogEventMatcher("format", "argument");
-		Description description = new StringDescription();
-
-		// When
-		matcher.describeTo(description);
-
-		// Then
-		assertThat(description.toString(), is("loggedInfo(\"format\", \"argument\")"));
-	}
 }
---
+++ src/test/java/uk/co/webamoeba/slf4j/junit/matcher/LogEventMatcherTest.java
@@ -0,0 +1,413 @@
+package uk.co.webamoeba.slf4j.junit.matcher;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.StringDescription;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.Marker;
+import org.slf4j.helpers.BasicMarkerFactory;
+
+import uk.co.webamoeba.slf4j.junit.event.LogEventRegistry;
+import uk.co.webamoeba.slf4j.junit.logger.RecordingLogger;
+
+/**
+ * @author James Kennard
+ */
+public abstract class LogEventMatcherTest {
+
+	@Before
+	public void clearAll() {
+		LogEventRegistry.getSingleton().clearAll();
+	}
+
+	@Test
+	public void shouldImplementMatcher() {
+		assertThat(matcher(), is(instanceOf(Matcher.class)));
+	}
+
+	@Test
+	public void shouldNotMatchGivenUnexpectedTypeOfLogger() {
+		// Given
+		LogEventMatcher matcher = matcher();
+		Logger logger = mock(Logger.class);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertFalse(matches);
+	}
+
+	@Test
+	public void shouldDescribeToGivenUnexpectedTypeOfLogger() {
+		// Given
+		LogEventMatcher matcher = matcher();
+		Logger logger = mock(Logger.class);
+		Description description = new StringDescription();
+
+		// When
+		matcher.describeMismatch(logger, description);
+
+		// Then
+		assertThat(description.toString(), is("the item is not a test logger, it looks like you are not using the matcher correctly "));
+	}
+
+	@Test
+	public void shouldMatchGivenLoggerHasMessage() {
+		// Given
+		String message = "Some Message";
+		LogEventMatcher matcher = matcher(message);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(message, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(true));
+	}
+
+	@Test
+	public void shouldMatchGivenLoggerHasMarkerAndMessage() {
+		// Given
+		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
+		String message = "Some Message";
+		LogEventMatcher matcher = matcher(marker, message);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(marker, message, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(true));
+	}
+
+	@Test
+	public void shouldMatchGivenLoggerHasMarkerAndFormattedMessage() {
+		// Given
+		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
+		String format = "Format {}";
+		String argument = "argument";
+		LogEventMatcher matcher = matcher(marker, format, argument);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(marker, format, argument, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(true));
+	}
+
+	@Test
+	public void shouldMatchGivenLoggerHasMessageAndThrowable() {
+		// Given
+		String message = "Some Message";
+		Throwable throwable = new Throwable();
+		LogEventMatcher matcher = matcher(message, throwable);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(message, throwable, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(true));
+	}
+
+	@Test
+	public void shouldMatchGivenLoggerHasMarkerAndMessageAndThrowable() {
+		// Given
+		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
+		String message = "Some Message";
+		Throwable throwable = new Throwable();
+		LogEventMatcher matcher = matcher(marker, message, throwable);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(marker, message, throwable, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(true));
+	}
+
+	@Test
+	public void shouldNotMatchGivenLoggerHasMessageButNoMarker() {
+		// Given
+		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
+		String message = "Some Message";
+		LogEventMatcher matcher = matcher(marker, message);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(message, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(false));
+	}
+	
+	@Test
+	public void shouldNotMatchGivenLoggerHasMarkerAndMessageButNoThrowable() {
+		// Given
+		Marker marker = new BasicMarkerFactory().getMarker("Some Marker");
+		String message = "Some Message";
+		Throwable throwable = new Throwable();
+		LogEventMatcher matcher = matcher(marker, message, throwable);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(marker, message, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(false));
+	}
+	
+	@Test
+	public void shouldNotMatchGivenLoggerHasMessageButNoThrowable() {
+		// Given
+		String message = "Some Message";
+		Throwable throwable = new Throwable();
+		LogEventMatcher matcher = matcher(message, throwable);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(message, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(false));
+	}
+	
+	@Test
+	public void shouldNotMatchGivenLoggerHasMessageAndUnexpectedThrowable() {
+		// Given
+		String message = "Some Message";
+		Throwable throwable = new Throwable();
+		LogEventMatcher matcher = matcher(message);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(message, throwable, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(false));
+	}
+
+	@Test
+	public void shouldMatchGivenLoggerHasStringMessageAndMatcherHasFormatedMessage() {
+		// Given
+		String message = "Some Message";
+		String format = "{}";
+		LogEventMatcher matcher = log(format, message);
+		Logger logger = new RecordingLogger("A recording logger");
+		log(message, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(true));
+	}
+
+	@Test
+	public void shouldMatchGivenLoggerHasFormattedMessageAndMatcherHasStringMessage() {
+		// Given
+		String message = "Some Message";
+		LogEventMatcher matcher = matcher(message);
+		Logger logger = new RecordingLogger("A recording logger");
+		String format = "{}";
+		log(format, message, logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(true));
+	}
+
+	@Test
+	public void shouldNotMatchGivenLoggerHasNoLogEvents() {
+		// Given
+		String message = "Some Message";
+		LogEventMatcher matcher = matcher(message);
+		Logger logger = new RecordingLogger("A recording logger");
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(false));
+	}
+
+	@Test
+	public void shouldNotMatchGivenLoggerHasLogEventWithDifferentMessage() {
+		// Given
+		LogEventMatcher matcher = matcher("Some Message");
+		Logger logger = new RecordingLogger("A recording logger");
+		log("Some Different Message", logger);
+
+		// When
+		boolean matches = matcher.matches(logger);
+
+		// Then
+		assertThat(matches, is(false));
+	}
+
+	@Test
+	public void shouldDescribeMismatchGivenMessage() {
+		// Given
+		String message = "Some Message";
+		LogEventMatcher matcher = matcher(message);
+		String loggerName = "A recording logger";
+		Logger logger = new RecordingLogger(loggerName);
+		Description description = new StringDescription();
+
+		// When
+		matcher.describeMismatch(logger, description);
+
+		// Then
+		assertThat(description.toString(), is("info to \"" + loggerName + "\" with message \"" + message
+				+ "\" was not logged "));
+	}
+	
+	@Test
+	public void shouldDescribeMismatchGivenMessageAndThrowable() {
+		// Given
+		String message = "Some Message";
+		Throwable throwable = new Throwable();
+		LogEventMatcher matcher = matcher(message, throwable);
+		String loggerName = "A recording logger";
+		Logger logger = new RecordingLogger(loggerName);
+		Description description = new StringDescription();
+
+		// When
+		matcher.describeMismatch(logger, description);
+
+		// Then
+		assertThat(description.toString(), is("info to \"" + loggerName + "\" with message \"" + message
+				+ "\" and throwable <" + throwable + "> was not logged "));
+	}
+	
+	@Test
+	public void shouldDescribeMismatchGivenSomethingElseWasLogged() {
+		// Given
+		String message = "Some Message";
+		String somethingElse = "Something Else";
+		LogEventMatcher matcher = matcher(message);
+		String loggerName = "A recording logger";
+		Logger logger = new RecordingLogger(loggerName);
+		log(somethingElse, logger);
+		Description description = new StringDescription();
+
+		// When
+		matcher.describeMismatch(logger, description);
+
+		// Then
+		assertThat(description.toString(), is("info to \"" + loggerName + "\" with message \"" + message
+				+ "\" was not logged; But these were logged info(\"" + somethingElse + "\") "));
+	}
+	
+	@Test
+	public void shouldDescribeMismatchGivenOtherThingsWasLogged() {
+		// Given
+		String message = "Some Message";
+		String anotherThing = "Another Thing";
+		String yetAnotherThing = "Yet Another Thing";
+		LogEventMatcher matcher = matcher(message);
+		String loggerName = "A recording logger";
+		Logger logger = new RecordingLogger(loggerName);
+		log(anotherThing, logger);
+		log(yetAnotherThing, logger);
+		Description description = new StringDescription();
+
+		// When
+		matcher.describeMismatch(logger, description);
+
+		// Then
+		assertThat(description.toString(), is("info to \"" + loggerName + "\" with message \"" + message
+				+ "\" was not logged; But these were logged info(\"" + anotherThing + "\") info(\"" + yetAnotherThing + "\") "));
+	}
+
+	@Test
+	public void shouldDescribeToGivenStringMessage() {
+		// Given
+		LogEventMatcher matcher = matcher("Some Message");
+		Description description = new StringDescription();
+
+		// When
+		matcher.describeTo(description);
+
+		// Then
+		assertThat(description.toString(), is("loggedInfo(\"Some Message\")"));
+	}
+	
+	@Test
+	public void shouldDescribeToGivenStringMessageAndThrowable() {
+		// Given
+		String message = "Some Message";
+		Throwable throwable = new Throwable();
+		LogEventMatcher matcher = matcher(message, throwable);
+		Description description = new StringDescription();
+
+		// When
+		matcher.describeTo(description);
+
+		// Then
+		assertThat(description.toString(), is("loggedInfo(\"Some Message\", <" + throwable + ">)"));
+	}
+	
+	@Test
+	public void shouldDescribeToGivenFormattedMessage() {
+		// Given
+		LogEventMatcher matcher = matcher("format", "argument");
+		Description description = new StringDescription();
+
+		// When
+		matcher.describeTo(description);
+
+		// Then
+		assertThat(description.toString(), is("loggedInfo(\"format\", \"argument\")"));
+	}
+	
+	protected abstract LogEventMatcher matcher();
+	
+	protected abstract LogEventMatcher matcher(String message);
+	
+	protected abstract LogEventMatcher matcher(String format, String argument);
+	
+	protected abstract LogEventMatcher matcher(Marker marker, String message);
+	
+	protected abstract LogEventMatcher matcher(String message, Throwable throwable);
+	
+	protected abstract LogEventMatcher matcher(Marker marker, String message, Throwable throwable);
+	
+	protected abstract LogEventMatcher matcher(Marker marker, String format, String argument);
+	
+	protected abstract void log(String message, Logger logger);
+	
+	protected abstract void log(String format, String argument, Logger logger);
+	
+	protected abstract void log(Marker marker, String message, Logger logger);
+	
+	protected abstract LogEventMatcher log(String format, String argument);
+	
+	protected abstract void log(String message, Throwable throwable, Logger logger);
+	
+	protected abstract void log(Marker marker, String message, Throwable throwable, Logger logger);
+	
+	protected abstract void log(Marker marker, String format, String argument, Logger logger);
+	
+}
